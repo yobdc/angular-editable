190eac3bb2777dd738c857ebb82bf4adb877a5cd
yobdc <yobdc.101@gmail.com>
2013-09-22 17:23:34 +0800

    切换只读模式到input模式

--- js/controllers.js
+++ js/controllers.js
@@ -7,4 +7,10 @@ controller('MyCtrl1', ['$scope', function($scope) {
 	$scope.due_date = new Date();
 	$scope.person_name = "John Doe";
 	$scope.address = "133, North Avenue";
+
+	$scope.handleInputBlur = function() {
+		var n = 0;
+	};
+
+	$('#zz')
 }]);
--- js/directives.js
+++ js/directives.js
@@ -26,4 +26,63 @@ directive('appVersion', ['version', function(version) {
 			}, 10);
 		}
 	};
-});
+}).directive('ngBlur', function() {
+	return function(scope, elem, attrs) {
+		elem.bind('blur', function() {
+			scope.$apply(attrs.ngBlur);
+		});
+	};
+}).directive('editable', ['$parse', function($parse) {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			mode: '@',
+			output: '='
+		},
+		transclude: true,
+		template: '<div><div ng-show="isEdit||mode==\'in\'" ng-transclude></div><div ng-show="mode==\'out\'||(mode==\'mix\'&&!isEdit)">output</div></div>',
+		link: function(scope, iElement, iAttrs, controller) {
+			scope.out = $parse(iAttrs.output);
+			var locals = {};
+			var model = scope.out(scope, locals);
+			var n = 0;
+		},
+		controller: function($scope, $element, $attrs, $transclude) {
+			$scope.isEdit = false;
+			$element.bind('dblclick', function(event) {
+				if($scope.mode == 'mix') {
+					$scope.isEdit = true;
+					$scope.$apply();
+					$("input")[0].focus();
+				};
+			});
+
+			var searchEle = function(array, target) {
+					var result = false;
+					for(var i = 0; i < array.length; i++) {
+						if(array[i] == target) {
+							result = true;
+							break;
+						};
+						if( !! array[i].children.length) {
+							result = searchEle(array[i].children, target);
+							if(result) {
+								break;
+							};
+						};
+					};
+					return result;
+				};
+
+			$(document).click(function(e) {
+				var tar = angular.element(e.target);
+				var result = searchEle($element.children(), tar[0]);
+				if(!result) {
+					$scope.isEdit = false;
+					$scope.$apply();
+				};
+			});
+		}
+	};
+}]);
