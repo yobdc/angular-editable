9a76956222a96ab629b94b9d07716421db76c94e
yobdc <yobdc.101@gmail.com>
2013-09-23 16:49:32 +0800

    保存取消事件

--- js/controllers.js
+++ js/controllers.js
@@ -7,10 +7,16 @@ controller('MyCtrl1', ['$scope', function($scope) {
 	$scope.due_date = new Date();
 	$scope.person_name = "John Doe";
 	$scope.address = "133, North Avenue";
+	$scope.product = {
+		name: '11',
+		number: '33'
+	};
 
 	$scope.handleInputBlur = function() {
 		var n = 0;
 	};
 
-	$('#zz')
+	$scope.show = function(){
+		alert($scope.product.name + ' ' + $scope.product.number);
+	};
 }]);
--- js/directives.js
+++ js/directives.js
@@ -37,19 +37,41 @@ directive('appVersion', ['version', function(version) {
 		restrict: 'E',
 		replace: true,
 		scope: {
-			mode: '@',
 			output: '=output'
 		},
+		// scope: true,
 		transclude: true,
-		template: '<div><div ng-show="isEdit||mode==\'in\'" ng-transclude></div><div ng-show="mode==\'out\'||(mode==\'mix\'&&!isEdit)">{{output}}</div></div>',
+		template: '<div>' + '<div ng-show="isEdit" ng-transclude></div>' + 
+		'<div ng-show="!isEdit">{{output}}</div>' + 
+		'<button ng-show="isEdit&&mode==\'mix\'" ng-click="ok()">OK</button>' + 
+		'<button ng-show="isEdit&&mode==\'mix\'" ng-click="cancel()">Cancel</button>' + 
+		'</div>',
 		link: function(scope, iElement, iAttrs, controller) {
-			scope.out = $parse(iAttrs.output);
-			var locals = {};
-			var model = scope.out(scope, locals);
-			var n = 0;
+			// scope.output = scope[iAttrs.output];
+			// scope.output = 1;
+			if(iAttrs.mode == 'in' || iAttrs.mode == 'out' || iAttrs.mode == 'mix') {
+				scope.mode = iAttrs.mode;
+			} else {
+				scope.mode = 'in';
+			};
+
+			if(iAttrs.mode == 'in') {
+				scope.isEdit = true;
+			} else {
+				scope.isEdit = false;
+			};
 		},
 		controller: function($scope, $element, $attrs, $transclude) {
-			$scope.isEdit = false;
+			// if ($attrs.mode=='in' || $attrs.mode=='out' || $attrs.mode=='mix') {
+			// 	$scope.mode = $attrs.mode;
+			// } else {
+			// 	$scope.mode = 'in';
+			// };
+			// if ($attrs.mode=='in') {
+			// 	$scope.isEdit = true;
+			// } else {
+			// 	$scope.isEdit = false;
+			// };
 			$element.bind('dblclick', function(event) {
 				if($scope.mode == 'mix') {
 					$scope.isEdit = true;
@@ -78,10 +100,55 @@ directive('appVersion', ['version', function(version) {
 			$(document).click(function(e) {
 				var tar = angular.element(e.target);
 				var result = searchEle($element.children(), tar[0]);
-				if(!result) {
-					$scope.isEdit = false;
+				if(!result && $scope.mode == 'mix') {
+					$scope.ok();
 					$scope.$apply();
 				};
+			});
+
+			$scope.ok = function() {
+				$scope.isEdit = false;
+				$scope.$parent.$broadcast('ok');
+			};
+
+			$scope.cancel = function() {
+				$scope.isEdit = false;
+				$scope.$parent.$broadcast('cancel');
+			};
+
+			if($scope.mode == 'in') {
+				$scope.$watch('mode', function(oldValue, newValue) {
+					var n = 0;
+				});
+			};
+		}
+	};
+}]).directive('edit', [function() {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, iElement, iAttrs, controller) {
+			// controller.$setViewValue(scope.model);
+			// iElement.val(scope.model);
+		},
+		controller: function($scope, $element, $attrs, $transclude) {
+			var scope = $scope;
+			var attrs = $attrs;
+			var copyAttr = function(des, src, attr) {
+					var list = attr.split('.');
+					des[list[0]] = angular.copy(src[list[0]]);
+				};
+			$element.bind('click', function(event) {
+				copyAttr(scope.$parent, scope, attrs.ngModel);
+			});
+			$scope.$on('ok', function(event, args) {
+				copyAttr(scope.$parent, scope, attrs.ngModel);
+				// scope.$parent.output = scope[attrs.ngModel];
+			});
+			$scope.$on('cancel', function(event, args) {
+				copyAttr(scope, scope.$parent, attrs.ngModel);
+				// scope[attrs.ngModel] = scope.$parent[attrs.ngModel];
+				// scope[attrs.ngModel] = scope.$parent.output;
 			});
 		}
 	};
