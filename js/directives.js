d9db9e17000959ab93925ebc662bd58340230e3d
yobdc <yobdc.101@gmail.com>
2013-09-26 20:59:17 +0800

    test

--- js/directives.js
+++ js/directives.js
@@ -3,7 +3,7 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).directive('editable', ['$compile', 'RandomService', '$templateCache', function($compile, RandomService, $templateCache) {
+angular.module('myApp.directives', []).directive('editable', ['$compile', 'RandomService', '$templateCache', '$http', function($compile, RandomService, $templateCache, $http) {
 	return {
 		restrict: 'E',
 		replace: true,
@@ -12,8 +12,12 @@ angular.module('myApp.directives', []).directive('editable', ['$compile', 'Rando
 		},
 		priority: 0,
 		transclude: true,
-		template: '<div>' + '<div ng-show="isEdit" ng-transclude></div>' + '<label class="control-label showhim" ng-show="!isEdit">{{output}}' + '&nbsp;&nbsp;&nbsp;<i class="icon-repeat showme" ng-show="!isEdit&&(mode==\'mix\'||mode==\'popover\')" ng-click="restore()"></i>' + '</label>' +
-		// '<script type="text/ng-template" id="zz.html" ng-transclude>111</script>' +
+		template: '<div>' + 
+		'<div ng-show="isEdit" ng-transclude></div>' + 
+		'<label class="control-label showhim" ng-show="!isEdit">{{output}}' + 
+		'&nbsp;&nbsp;&nbsp;<i class="icon-repeat showme" ng-show="!isEdit&&(mode==\'mix\'||mode==\'popover\')" ng-click="restore()"></i>' + 
+		'</label>' +
+		// '<script type="text/ng-template" id="{{tmpId}}" ng-transclude>111</script>' +
 		// '<button type="button" class="btn" bs-popover="\'zz.html\'">aaa</button>'+
 		// '<button ng-show="isEdit&&mode==\'mix\'" ng-click="ok()">OK</button>' + 
 		// '<button ng-show="isEdit&&mode==\'mix\'" ng-click="cancel()">Cancel</button>' + 
@@ -21,21 +25,21 @@ angular.module('myApp.directives', []).directive('editable', ['$compile', 'Rando
 		compile: function compile(tElement, tAttrs, transclude) {
 			return {
 				pre: function(scope, iElement, iAttrs, controller) {
-					scope.tmpId = 'TMP' + RandomService.string() + '.html'
-					scope.ele = iElement;
-					scope.includeHtml = $('<div><button type="button" class="btn" bs-popover="\''+scope.tmpId+'\'">aaa</button>' + '<script type="text/ng-template" id="' + scope.tmpId + '">zz</script></div>');
-					scope.ready = false;
-					transclude(scope, function(jElement, jScope){
-						jScope.includeHtml[0].children[1].appendChild(jElement[1]);
-						jScope.ele[0].appendChild(jScope.includeHtml[0]);
-						$templateCache.put(jScope.tmpId, jScope.includeHtml[0].children[1].innerHTML);
-						var a = $templateCache.get(jScope.tmpId);
-						jScope.ready = true;
-						$compile(angular.element(jScope.includeHtml[0]))(jScope);
-					});
-					while(!scope.ready){
-						continue;
-					}
+					// scope.tmpId = 'TMP' + RandomService.string() + '.html'
+					// scope.ele = iElement;
+					// scope.includeHtml = $('<div><button type="button" class="btn" bs-popover="\''+scope.tmpId+'\'">aaa</button>' + '<script type="text/ng-template" id="' + scope.tmpId + '">zz</script></div>');
+					// scope.ready = false;
+					// transclude(scope, function(jElement, jScope){
+					// 	// jScope.includeHtml[0].children[1].appendChild(jElement[1]);
+					// 	angular.element(jScope.includeHtml[0].children[1]).append(angular.element(jElement[1]));
+					// 	angular.element(jScope.ele[0]).append(angular.element(jScope.includeHtml[0]));
+					// 	// jScope.ele[0].appendChild(jScope.includeHtml[0]);
+					// 	$templateCache.put(jScope.tmpId, jScope.includeHtml[0].children[1].innerHTML);
+					// 	var a = $templateCache.get(jScope.tmpId);
+					// 	jScope.tmgIdCache = a;
+					// 	jScope.ready = true;
+					// 	$compile(angular.element(jScope.ele[0].children[2].children[0]))(jScope);
+					// });
 					// scope.tmpId = 'TMP' + RandomService.string() + '.html'
 					// scope.ele = iElement;
 					// scope.includeHtml = $('<div><button type="button" class="btn" bs-popover="\''+scope.tmpId+'\'">aaa</button>' + '<script type="text/ng-template" id="' + scope.tmpId + '" ng-transclude>zz</script></div>');
@@ -55,7 +59,20 @@ angular.module('myApp.directives', []).directive('editable', ['$compile', 'Rando
 					} else {
 						scope.isEdit = false;
 					};
-					// $templateCache.put('zz.html', iElement[0].children[2].innerHTML);
+					// scope.tmpId = 'TMP' + RandomService.string() + '.html'
+					// scope.ele = iElement;
+					// scope.includeHtml = $('<button type="button" class="btn" bs-popover="\''+scope.tmpId+'\'">aaa</button>');
+					// // scope.includeHtml[0].children[1].appendChild(scope.ele[0]);
+					// scope.ele[0].appendChild(scope.includeHtml[0]);
+					// // $compile(angular.element(scope.ele[0].children[3]))(scope);
+					// // $compile(angular.element(scope.ele[0].children[2]))(scope);
+					// $http.get(scope.tmpId).then(function(data) {
+					// 	// body...
+					// 	var n = 0;
+					// },function(data) {
+					// 	// body...
+					// 	var n = $templateCache.get(scope.tmpId);
+					// });
 				}
 			}
 		},
@@ -117,6 +134,95 @@ angular.module('myApp.directives', []).directive('editable', ['$compile', 'Rando
 					element: element
 				});
 			};
+		}
+	};
+}]).directive('editable2', ['$compile', 'RandomService', '$templateCache', '$http', function($compile, RandomService, $templateCache, $http) {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			output: '=output',
+		},
+		priority: 0,
+		transclude: true,
+		template: '<div>' + 
+		'<div ng-transclude></div>' + 
+		'<label class="control-label showhim" bs-popover="\'{{tmpId}}\'">{{output}}' + 
+		'&nbsp;&nbsp;&nbsp;<i class="icon-repeat showme" ng-show="!isEdit" ng-click="restore()"></i>' + 
+		'</label>' +
+		// '<script type="text/ng-template" id="{{tmpId}}" ng-transclude>111</script>' +
+		// '<button type="button" class="btn" bs-popover="\'a.html\'">aaa</button>'+
+		// '<button ng-show="isEdit&&mode==\'mix\'" ng-click="ok()">OK</button>' + 
+		// '<button ng-show="isEdit&&mode==\'mix\'" ng-click="cancel()">Cancel</button>' + 
+		'</div>',
+		compile: function compile(tElement, tAttrs, transclude) {
+			return {
+				pre: function(scope, iElement, iAttrs, controller) {
+					// scope.tmpId = 'TMP' + RandomService.string() + '.html';
+					scope.tmpId = iAttrs.tmpid;
+				},
+				post: function(scope, iElement, iAttrs, controller) {
+				}
+			}
+		},
+		controller: function($scope, $element, $attrs, $transclude) {
+			var element = $element;
+			$element.bind('dblclick', function(event) {
+				// if($scope.mode == 'mix') {
+				// 	$scope.isEdit = true;
+				// 	$scope.$apply();
+				// 	$("input")[0].focus();
+				// };
+			});
+
+			var searchEle = function(array, target) {
+					var result = false;
+					for(var i = 0; i < array.length; i++) {
+						if(array[i] == target) {
+							result = true;
+							break;
+						};
+						if( !! array[i].children.length) {
+							result = searchEle(array[i].children, target);
+							if(result) {
+								break;
+							};
+						};
+					};
+					return result;
+				};
+
+			// $(document).click(function(e) {
+			// 	var tar = angular.element(e.target);
+			// 	var result = searchEle($element.children(), tar[0]);
+			// 	if(!result && $scope.isEdit && ($scope.mode == 'mix' || $scope.mode == 'in')) {
+			// 		$scope.ok();
+			// 		$scope.$apply();
+			// 	};
+			// });
+
+			// $scope.ok = function() {
+			// 	if($scope.mode == 'mix') {
+			// 		$scope.isEdit = false;
+			// 	};
+			// 	$scope.$parent.$broadcast('ok', {
+			// 		element: element
+			// 	});
+			// };
+
+			// $scope.cancel = function() {
+			// 	if($scope.mode == 'mix') {
+			// 		$scope.isEdit = false;
+			// 	};
+			// 	$scope.$parent.$broadcast('cancel', {
+			// 		element: element
+			// 	});
+			// };
+			// $scope.restore = function() {
+			// 	$scope.$parent.$broadcast('revert', {
+			// 		element: element
+			// 	});
+			// };
 		}
 	};
 }]).directive('edit', [function() {
