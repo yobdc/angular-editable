06b288bb70c46dc4470f3d3ac43b54e99b1c0634
yobdc <yobdc.101@gmail.com>
2013-09-26 16:21:06 +0800

    随即id生成service

--- js/services.js
+++ js/services.js
@@ -6,4 +6,14 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+value('version', '0.1').service('RandomService', function() {
+	return {
+		string: function() {
+			var text = "";
+			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			for(var i = 0; i < 5; i++)
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+			return text;
+		}
+	};
+});
